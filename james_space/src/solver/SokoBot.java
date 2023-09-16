e8c8a4280279a6243a653590a8a3ca926b4f7211
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2023-09-16 12:49:34 +0000

    spaces initialized

--- james_space/src/solver/SokoBot.java
+++ james_space/src/solver/SokoBot.java
@@ -1,22 +1,51 @@
 package solver;
 
 public class SokoBot {
-
+  // Lets try implementing a naive solution
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    /*
-     * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
-     */
-    /*
-     * Default stupid behavior: Think (sleep) for 3 seconds, and then return a
-     * sequence
-     * that just moves left and right repeatedly.
-     */
-    try {
-      Thread.sleep(3000);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
     return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
+  }
+
+  /*
+   * Generates goal data.
+   * 
+   * @param width   width of the map
+   * @param height  height of the map
+   * @param mapData 2D character array containing data about immovable objects in the map
+   * 
+   * @return a 2D character array representing the goal state
+   */
+  public char[][] generateGoalData (int width, int height, char[][] mapData) {
+    char[][] goalData = mapData;
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        goalData[i][j] = (goalData[i][j] == '.')? '*' : goalData[i][j];
+      }
+    }
+    return goalData;
+  }
+
+  /*
+   * Gets the players coordinates.
+   * 
+   * @param width     width of the map
+   * @param height    height of the map
+   * @param itemsData 2D character array containing data about movable objects on the map
+   * 
+   * @return an integer array <coordinates> where <coordinates = {x-coordinate, y-coordinate}>
+   */
+  public int[] getPlayerCoordinates (int width, int height, char[][] itemsData) {
+    int[] coordinates = {0, 0};
+    while (itemsData[coordinates[0]][coordinates[1]] != '@' && coordinates[0] < height) {
+      while(itemsData[coordinates[0]][coordinates[1]] != '@' && coordinates[1] < width) {
+        coordinates[1]++;
+      }
+      if (itemsData[coordinates[0]][coordinates[1]] != '@') {
+        coordinates[0]++;
+        coordinates[1] = 0;
+      }
+    }
+    return coordinates;
   }
 
 }

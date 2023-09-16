cfd08f6f908a492aa261e7aca5c2a8713407904a
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2023-09-16 14:18:04 +0000

    my stuff, might cull some spaces later for optimality

--- james_space/src/solver/SokoBot.java
+++ james_space/src/solver/SokoBot.java
@@ -1,7 +1,10 @@
 package solver;
 
+import java.util.*;
+import java.lang.*;
+
 public class SokoBot {
-  // Lets try implementing a naive solution
+
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
     return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
   }
@@ -48,4 +51,78 @@ public class SokoBot {
     return coordinates;
   }
 
+  /*
+   * Checks which moves are possible in which cardinal directions.
+   * 
+   * 
+   */
+  public String checkMove (int width, int height, char[][] mapData, char[][] itemsData, int[] coordinates) {
+    String rString = "";
+    int row = coordinates[0];
+    int col = coordinates[1];
+
+    // checks in the northern direction
+    if (row >= 1) {
+      if (mapData[row - 1][col] == ' ') {
+        rString += "u";
+      }
+      else if (itemsData[row - 1][col] == '$' && row >= 2) {
+        if (mapData[row - 2][col] == ' ') {
+          rString += "u";
+        }
+      } 
+    }
+
+    // checks in the southern direction
+    if (row < height - 1) {
+      if (mapData[row + 1][col] == ' ') {
+        rString += "d";
+      }
+      else if (itemsData[row + 1][col] == '$' && row < height - 2) {
+        if (mapData[row + 2][col] == ' ') {
+          rString += "d";
+        }
+      }
+    }
+
+    // checks in the western direction
+    if (col >= 1) {
+      if (mapData[row][col - 1] == ' ') {
+        rString += "l";
+      }
+      else if (itemsData[row][col - 1] == '$' && col >= 2) {
+        if (mapData[row][col - 2] == ' ') {
+          rString += "l";
+        }
+      }
+    }
+
+    // checks in the eastern direction
+    if (col < width - 1) {
+      if (mapData[row][col + 1] == ' ') {
+        rString += 'r';
+      }
+      else if (itemsData[row][col + 1] == '$' && col < width - 2) {
+        if (mapData[row][col + 2] == ' ') {
+          rString += "r";
+        } 
+      }
+    }
+
+    return rString;
+  }
+
+  /*
+   * Models moving behaviour.
+   */
+  public char[][] move (int width, int height, char[][] ) {
+    
+  }
+  /*
+   * Models a naive "backtracking" solution.
+   */
+  public String naiveSolution () {
+
+    return "";
+  }
 }

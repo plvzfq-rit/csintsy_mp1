edbe11a15204065fa38d7bb1dae79898f07faa79
bewba <67249789+bewba@users.noreply.github.com>
2023-09-17 16:59:51 +0800

    Add files via upload
    Added more helper functions

--- hans_space/src/solver/SokoBot.java
+++ hans_space/src/solver/SokoBot.java
@@ -1,7 +1,11 @@
 package solver;
+import java.lang.reflect.Array;
 import java.util.*;
-public class SokoBot {
 
+
+public class SokoBot {
+  public static final char WALL = '#', EMPTY = ' ', TARGET = '.', BOX = '$', PLAYER = '@';
+  //Initialization
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
     /*
      * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
@@ -12,9 +16,9 @@ public class SokoBot {
      * that just moves left and right repeatedly.
      */
     
-    int[] Playerloc = findPLayerLoc(height, width, itemsData);
-    System.out.println(Playerloc[0]);
-    System.out.println(Playerloc[1]);
+    int[] playerloc = findPLayerLoc(height, width, itemsData);
+    System.out.println(playerloc[0]);
+    System.out.println(playerloc[1]);
 
     ArrayList<int[]> boxes = findBoxLocations(height, width, itemsData);
     for(int k = 0; k < boxes.size(); k++){
@@ -27,13 +31,57 @@ public class SokoBot {
     } catch (Exception ex) {
       ex.printStackTrace();
     }
-    return "dududududud";
+
+    
+    return solve(width,height,mapData,itemsData, playerloc[0],playerloc[1], boxes, "",0,0);
+  }
+
+  public static String solve(int width, int height, char[][] mapData, char[][] itemsData, int playerX, int playerY,ArrayList<int[]> boxes, String sol, int changeInX, int changeinY){
+
+    return sol;
+  }
+  /*
+   *  Sorts Arrays by Row then Column
+   */
+  public static ArrayList<int[]> sort(ArrayList<int[]> boxes){ 
+    ArrayList<int[]> res = new ArrayList<>(); 
+    res.add(boxes.get(0));
+    for(int i = 1; i < boxes.size()-1; i++){
+      int[] currEl = boxes.get(i);
+      int[] nextEl = boxes.get(i+1);
+      int[] temparr;
+      if(currEl[0] > nextEl[0]){
+        //swap(currEl and nextEl)
+        temparr = currEl.clone();
+        currEl = nextEl.clone();
+        nextEl = temparr.clone();
+        res.add(currEl);
+        res.add(nextEl);
+        i++;
+      } else if (currEl[0] == nextEl[0] && currEl[1] > nextEl[1]){
+        //swap(currEl and nextEl)
+        temparr = currEl.clone();
+        currEl = nextEl.clone();
+        nextEl = temparr.clone();
+        res.add(currEl);
+        res.add(nextEl);
+        i++;
+      } else {
+        res.add(currEl);
+      }
+    }
+    return res;
+  }
+  //Make a key for the queue to ensure that there wont be repetittion
+  public static String makeKey(String a){
+    
+    return a;
   }
 
   public static boolean isSolved(int height,int width,char[][] mapData, char[][] itemsData){
     for(int i = 0; i < height; i++){
       for(int j = 0; j < width; j++){
-        if(mapData[i][j] == '.' && itemsData[i][j] != '$'){
+        if(mapData[i][j] == TARGET && itemsData[i][j] != BOX){
           return false;
         }
       }
@@ -46,7 +94,7 @@ public class SokoBot {
     int[] location = new int[2];
     for(i = 0; i < height; i++){
       for(j = 0; j < width; j++){
-        if(itemsData[i][j] == '@'){
+        if(itemsData[i][j] == PLAYER){
           location[0] = i;
           location[1] = j;
           return location;
@@ -63,7 +111,7 @@ public class SokoBot {
     int[] location = new int[2];
     for(i = 0; i < height; i++){
       for(j = 0; j < width; j++){
-        if(itemsData[i][j] == '$'){
+        if(itemsData[i][j] == BOX){
           System.out.println("Box at: "+ i+ ", " + j);
           location[0] = i;
           location[1] = j;
